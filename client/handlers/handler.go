c2600416576dbb9277ffdcbd69d1ac0b74805e07
endlife93 <475181585@qq.com>
2022-01-27 22:24:45 +0800

    19熔断器初步整合

--- client/handlers/handler.go
+++ client/handlers/handler.go
@@ -2,38 +2,60 @@ package handlers
 
 import (
 	"context"
-	"github.com/afex/hystrix-go/hystrix"
 	"github.com/charlie/gomicroconsul/Services"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
+func newProd(id int32, pname string) *Services.ProdModel {
+	return &Services.ProdModel{ProdID: id, ProdName: pname}
+}
 
+// 降级后的默认商品
+func defaultProds()(*Services.ProdListResponse,error) {
+	models := make([]*Services.ProdModel, 0)
+	var i int32
+	for i = 0; i < 5; i++ {
+		models = append(models, newProd(20+i, "prodName"+strconv.Itoa(100+int(i))))
+	}
+	res := &Services.ProdListResponse{}
+	res.Data = models
+	return res,nil
+}
 // GetProdList 显示商品列表
 func GetProdList(c *gin.Context) {
 	prodService := c.Keys["ProdService"].(Services.ProdService)
 	var prodReq Services.ProdRequest
 	err := c.Bind(&prodReq)
 	if err != nil {
-		c.JSON(500, gin.H{
-			"status": err.Error()})
+		c.JSON(500, gin.H{"status": err.Error()})
 	} else {
-		// 熔断器代码改造
-		// 1.配置config
-		configA := hystrix.CommandConfig{
-			Timeout: 5000,
-		}
-		// 2.配置command
-		hystrix.ConfigureCommand("getProds",configA)
-		// 3. 执行Do 方法
-		var prodRes * Services.ProdListResponse
-		err := hystrix.Do("getProds", func() error {
-			prodRes,err = prodService.GetProdList(context.Background(),&prodReq)
-			return err
-		},nil)
-		if err != nil {
-			c.JSON(500, gin.H{"status": err.Error()})
-		} else {
-			c.JSON(200, gin.H{"data": prodRes.Data})
-		}
+		prodRes, _ := prodService.GetProdList(context.Background(), &prodReq)
+		c.JSON(200, gin.H{"data": prodRes.Data})
 	}
+
+
+
+	//if err != nil {
+	//	c.JSON(500, gin.H{
+	//		"status": err.Error()})
+	//} else {
+	//	// 熔断器代码改造
+	//	// 1.配置config
+	//	configA := hystrix.CommandConfig{
+	//		Timeout: 5000,
+	//	}
+	//	// 2.配置command
+	//	hystrix.ConfigureCommand("getProds",configA)
+	//	// 3. 执行Do 方法
+	//	var prodRes * Services.ProdListResponse
+	//	err := hystrix.Do("getProds", func() error {
+	//		prodRes,err = prodService.GetProdList(context.Background(),&prodReq)
+	//		return err
+	//	}, func(err error) error {
+	//		prodRes,err = defaultProds()
+	//		return err
+	//	})
+	//
+	//}
 
 }
--- client/webclient/main.go
+++ client/webclient/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/charlie/gomicroconsul/Services"
+	"github.com/charlie/gomicroconsul/Wrappers"
 	"github.com/charlie/gomicroconsul/client/Weblib"
 	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/client"
@@ -12,6 +13,7 @@ import (
 	"github.com/micro/go-micro/v2/web"
 	"github.com/micro/go-plugins/registry/consul/v2"
 )
+
 // 使用go-micro官方提供的wrapper来对路由进行装饰
 type logWrapper struct {
 	client.Client
@@ -27,26 +29,26 @@ func (l *logWrapper) Call(ctx context.Context, req client.Request, rsp interface
 func newLogWrapper(c client.Client) client.Client {
 	return &logWrapper{c}
 }
+
 // 在gin 中调用上节课构建的rpc服务
-func main()  {
+func main() {
 
 	consulReg := consul.NewRegistry(
 		registry.Addrs("127.0.0.1:8500"),
-		)
+	)
 
 	myService := micro.NewService(
 		micro.Name("prodservice.client"),
-		micro.WrapClient(newLogWrapper),  // 使用装饰器
-
-		)
-	prodService := Services.NewProdService("ProdService",myService.Client())
+		micro.WrapClient(newLogWrapper), // 使用装饰器
+		micro.WrapClient(Wrappers.NewProdsWrapper),
+	)
+	prodService := Services.NewProdService("ProdService", myService.Client())
 	service := web.NewService(
 		web.Name("ProdService.client"),
 		web.Address(":9000"),
 		web.Handler(Weblib.InitRouter(prodService)),
 		web.Registry(consulReg),
-		)
+	)
 	service.Init()
 	service.Run()
 }
-
